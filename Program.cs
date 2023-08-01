b39049605e4a85d3d81582f14a17bd49255fc647
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-08-01 09:01:57 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppLinearSearch
+{
+    internal class Program
+    {
+        public static int LinearSearch(int[] arr, int Sitem)
+        {
+            int index = -1;
+            for(int i=0;i<arr.Length; i++)
+            {
+                if (arr[i] == Sitem)
+                {
+                    return i;
+                }
+            }
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 20, 5, 15 };
+            Console.WriteLine("Please Enter the Number you want to Enter");
+            int Sitem = int.Parse(Console.ReadLine());
+            int result = LinearSearch(arr, Sitem);
+            if (result != -1)
+            {
+                Console.WriteLine($"{Sitem} found at Index {result}");
+            }
+            else
+            {
+                    Console.WriteLine($"{Sitem} not Found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
